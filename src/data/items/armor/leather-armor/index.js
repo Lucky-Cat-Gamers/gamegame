6042f7d8995c6a7f68d3c802032105dd851fc66f
Krista Clarke <kristaclarke82@gmail.com>
2020-09-09 22:59:34 -0700

    created a word doc showing all the folders and files in the game structure

---
+++ src/data/items/armor/leather-armor/index.js
@@ -0,0 +1,11 @@
+import LeatherArmorImg from './leather-armor.png';
+
+const LeatherArmor = {
+  name: 'Leather Armor',
+  type: 'armor::body',
+  defence: 5,
+  image: LeatherArmorImg,
+  value: 40
+};
+
+export default LeatherArmor;
