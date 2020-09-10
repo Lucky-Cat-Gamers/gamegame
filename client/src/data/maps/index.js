a95912ecc599198571c1b3cad8c3cd10673c7e51
Krista Clarke <kristaclarke82@gmail.com>
2020-09-10 12:58:49 -0700

    moved all orig rpg game files into client folder in MVC structure & added .gitignore with node modules

---
+++ client/src/data/maps/index.js
@@ -0,0 +1,27 @@
+/*eslint-disable camelcase*/
+import map1_1 from './1_1';
+import map1_2 from './1_2';
+import map1_3 from './1_3';
+import map1_4 from './1_4';
+import map1_5 from './1_5';
+import map2_1 from './2_1';
+import map2_2 from './2_2';
+import map2_3 from './2_3';
+import map2_4 from './2_4';
+import map2_5 from './2_5';
+
+const maps = {
+  '1_1': map1_1,
+  '1_2': map1_2,
+  '1_3': map1_3,
+  '1_4': map1_4,
+  '1_5': map1_5,
+  '2_1': map2_1,
+  '2_2': map2_2,
+  '2_3': map2_3,
+  '2_4': map2_4,
+  '2_5': map2_5
+};
+/*eslint-enable camelcase*/
+
+export default maps;
---
+++ client/src/data/monsters/index.js
@@ -0,0 +1,17 @@
+import Dragon     from './dragon';
+import Goblin     from './goblin';
+import Imp        from './imp';
+import Lich       from './lich';
+import Rat        from './rat';
+import StoneGolem from './stone-golem';
+
+const monsters = {
+  dragon: Dragon,
+  goblin: Goblin,
+  imp: Imp,
+  lich: Lich,
+  rat: Rat,
+  'stone-golem': StoneGolem
+};
+
+export default monsters;
