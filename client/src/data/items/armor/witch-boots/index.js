422860b9d64ab38a2511852f1427450689d84e5a
Krista Clarke <kristaclarke82@gmail.com>
2020-09-13 09:21:55 -0700

    fixed: fixed images for witch hat, cape (need to rename cloak), gloves and boots. also added defense points until we can resolve tool/use scenario.

--- client/src/data/items/armor/witch-boots/index.js
+++ client/src/data/items/armor/witch-boots/index.js
@@ -1,8 +1,9 @@
-import witchBootsImg from './witchBoots.jpg';
+import witchBootsImg from './witch_boots.png';
 
 const witchBoots = {
   name: 'Witch Boots',
   type: 'armor::boots',
+  defence: 4
 //   protection: spiders, spiderWebs,
   image: witchBootsImg,
   value: 120
