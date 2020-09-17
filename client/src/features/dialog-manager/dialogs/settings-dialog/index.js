5e2c8889697a14a38fe74f42b20683fc729f4bf8
Mark F <the.mark.fullton@gmail.com>
2020-09-17 15:00:22 -0700

    Added save post button to settings

--- client/src/features/dialog-manager/dialogs/settings-dialog/index.js
+++ client/src/features/dialog-manager/dialogs/settings-dialog/index.js
@@ -1,47 +1,58 @@
-import React, { useState } from 'react';
-import { connect }         from 'react-redux';
-import GameMusic    from '../../../game-menus/game-music';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import GameMusic from "../../../game-menus/game-music";
+import axios from "axios";
 
-import Button         from '../../../../components/button';
-import ConfirmDialog  from '../../../../components/confirm-dialog';
-import Dialog         from '../../../../components/dialog';
-import closeSettings  from '../../actions/close-settings';
-import resetGameState from '../../../world/actions/reset-game-state';
+import Button from "../../../../components/button";
+import ConfirmDialog from "../../../../components/confirm-dialog";
+import Dialog from "../../../../components/dialog";
+import closeSettings from "../../actions/close-settings";
+import resetGameState from "../../../world/actions/reset-game-state";
 
-import './settings-dialog.scss';
+import "./settings-dialog.scss";
+
+const SaveScore = () => {
+  return (dispatch, getState) => {
+    const { level, gold } = getState().stats;
+
+    axios.post("/save", {
+      level: level,
+      gold: gold,
+    });
+  };
+};
 
 const SettingsDialog = ({ resetGameState, closeSettings }) => {
-
   const [confirmQuit, setConfirmQuit] = useState(false);
 
-  return(
+  return (
     <Dialog>
-      <div className='flex-column settings-dialog__container'>
-        <span className='settings-dialog__title'>
-          {'Settings'}
-        </span>
-
-        
+      <div className="flex-column settings-dialog__container">
+        <span className="settings-dialog__title">{"Settings"}</span>
 
         <Button
           onClick={() => setConfirmQuit(true)}
-          icon='caret-square-left'
-          title='Return to Menu' />
+          icon="caret-square-left"
+          title="Return to Menu"
+        />
+
+        <Button
+          onClick={SaveScore}
+          icon="pen-nib"
+          title="Save Your Current Score"
+        />
 
         <GameMusic />
 
-        <Button
-          onClick={closeSettings}
-          icon='times'
-          title='Close' />
+        <Button onClick={closeSettings} icon="times" title="Close" />
       </div>
 
       <ConfirmDialog
         open={confirmQuit}
-        text='Are you sure you want to quit? You will lose all progress...'
+        text="Are you sure you want to quit? You will lose all progress..."
         onClose={() => setConfirmQuit(false)}
-        confirm={resetGameState} />
-
+        confirm={resetGameState}
+      />
     </Dialog>
   );
 };
