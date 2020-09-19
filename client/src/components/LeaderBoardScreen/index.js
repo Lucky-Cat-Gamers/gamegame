97f183073a86b40c7cbce3cca8680fe1d6a12e43
Mark <the.mark.fullton@gmail.com>
2020-09-18 19:05:35 -0700

    Added old info to see if we can get it working

--- client/src/components/LeaderBoardScreen/index.js
+++ client/src/components/LeaderBoardScreen/index.js
@@ -6,41 +6,49 @@ class LeaderboardScreen extends Component {
     scores: [],
   };
 
-  // componentDidMount() {
-  //   console.log("Is running component did mount");
+  componentDidMount() {
+    console.log("Is running component did mount");
 
-  //   axios.get("https://witchfall.herokuapp.com/load").then((resp) => {
-  //     console.log(resp);
+    axios.get("https://witchfall.herokuapp.com/load").then((resp) => {
+      console.log(resp);
 
-  //     this.setState({
-  //       scores: resp,
-  //     });
-  //   });
-  // }
+      this.setState({
+        scores: resp.data,
+      });
+    });
+  }
 
   render() {
     return (
       <div>
-        <h3>High Scores</h3>
-        {/* <table>
-          <thead>
-            <tr>
-              <th>Player</th>
-              <th>Score</th>
-              <th>Gold</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.scores.map((scores, i) => (
+        <h2>High Scores</h2>
+        {this.state.scores.length ? (
+          <table>
+            <thead>
               <tr>
-                <td>{i + 1}</td>
-                <td>{scores.user}</td>
-                <td>{scores.level}</td>
-                <td>{scores.gold}</td>
+                <th>Player</th>
+                <th>Score</th>
+                <th>Gold</th>
               </tr>
-            ))}
-          </tbody>
-        </table> */}
+            </thead>
+            <tbody>
+              {this.state.scores.map((scores, i) => (
+                <tr>
+                  <td>{i + 1}</td>
+                  <td>{scores.user}</td>
+                  <td>{scores.level}</td>
+                  <td>{scores.gold}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <h1>
+            <br />
+            No Scores
+            <br />
+          </h1>
+        )}
       </div>
     );
   }
